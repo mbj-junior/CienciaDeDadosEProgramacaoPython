462718a1fe05124f646c275252f2897967475da9
Marcio Botelho Junior <marcio.junior@actdigital.com>
2023-03-22 18:18:59 -0300

    cd

---
+++ Aula-03/exercicio12.py
@@ -0,0 +1,25 @@
+"""
+12) Desenvolver um algoritmo e efetuar a implementação em Python para calcular o valor
+da função f(x). Atenção: NÃO É PERMITIDO EFETUAR A DIVISÃO POR ZERO.
+
+f(x) = (4 * x**2 - 3 * x + 9)/x 
+
+"""
+def calculate():
+    """Function to calculate."""
+    result = (4 * (number_x**2) - 3 * (number_x + 9))/number_x
+    return result
+
+
+
+while True:
+    try:
+        number_x = float(input("Informe x: "))
+        if number_x == 0:
+            raise ValueError("Não pode 0")
+    except ValueError as error:
+        print("Valor inválido: ", error)
+    else:
+        break
+
+print(f"Resultado da função: {calculate()}")
