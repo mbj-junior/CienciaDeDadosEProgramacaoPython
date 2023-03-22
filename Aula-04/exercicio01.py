2f9588a80ef191935119f8366b2bd6f9602f61fe
Marcio Botelho Junior <marcio.junior@actdigital.com>
2023-03-22 18:27:12 -0300

    feat: indice to exercises 01 and 02

---
+++ Aula-04/exercicio01.py
@@ -0,0 +1,14 @@
+"""
+1) Os comerciantes do Mercado Municipal de Rapidópolis estabeleceram 
+um percentual de margem mínima de lucro em função do valor do produto:
+
+Valor do Produto (VP) < R$ 25,00        Lucro de 100%
+R$ 25,00 ≤ VP < R$ 100,00               Lucro de 70%
+R$ 100,00 ≤ VP < R$ 500,00              Lucro de 60%
+R$ 500,00 ≤ VP < R$ 1000,00             Lucro de 50%
+VP ≥ R$ 1000,00                         Lucro de 40%
+
+Desenvolver um algoritmo para efetuar a leitura do valor do produto 
+e apontar o percentual mínimo de lucro. Efetue a implementação em 
+Python tratando exceções de dados durante a execução do programa.
+"""
---
+++ Aula-04/exercicio02.py
@@ -0,0 +1,5 @@
+"""
+2) Desenvolva um algoritmo e efetue a implementação em Python para 
+ler n valores (sendo n informado pelo usuário) e imprimir o valor médio, 
+o maior e o menor valor.
+"""
