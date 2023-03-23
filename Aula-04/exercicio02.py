258e8a41e58bb234fa5839491227e1b1b8b190d1
Marcio Botelho Junior <marcio.junior@actdigital.com>
2023-03-23 09:19:02 -0300

    feat: done exercise 02

--- Aula-04/exercicio02.py
+++ Aula-04/exercicio02.py
@@ -3,3 +3,39 @@
 ler n valores (sendo n informado pelo usuário) e imprimir o valor médio, 
 o maior e o menor valor.
 """
+
+numbers = []
+
+def average(list_numbers):
+    """Calculate the avarege of a list"""
+    return sum(list_numbers) / len(list_numbers)
+
+while True:
+    try:
+        number_repetitions = int(input("Quantidade de numeros: "))
+        if number_repetitions == 0:
+            raise RecursionError("Não pode ser 0")
+        if number_repetitions < 0:
+            raise RecursionError("Não pode ser negativo")
+        break
+    except RecursionError as error:
+        print("Valor inválido:", error)
+    except ValueError as error:
+        print("Utilizar somente numeros")
+    continue
+
+
+for i in range(number_repetitions):
+    while True:
+        try:
+            numbers.append(float(input(f"{i+1}º numero: ")))
+            break
+        except ValueError:
+            print("Por favor utilizar somente numeros...")
+    continue
+
+print("")
+print(f"Valor médio: {average(numbers)}")
+print(f"Maior valor: {max(numbers)}")
+print(f"Menor valor: {min(numbers)}")
+print("")
